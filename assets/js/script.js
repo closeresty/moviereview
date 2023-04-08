5ee13672723776d821ef3e593eff353cafc8f4b1
devres <potolinresty110394@gmail.com>
2023-04-08 13:01:24 +0800

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+
+$(document).ready(function(){
+    
+    $('.search-toggle').click(function(){
+        $('.search-wrapper').toggleClass('show');
+    });
+
+    $('.modal-toggle').click(function(){
+        $('.modalBox').toggleClass('show');
+    })
+
+    $('.modalBox').click(function(){
+        $(this).removeClass('show');
+    });
+
+    $('.spinner').click(function(){
+        $(".theme-selector").toggleClass('show');
+    });
+    $('.light').click(function(){
+        $('body').addClass('light-theme');
+        $('body').removeClass('dark-theme');
+    });
+    $('.dark').click(function(){
+        $('body').toggleClass('dark-theme');
+        $('body').removeClass('light-theme');
+    });
+});
+
+
+
+// smooth scroll
+$(document).ready(function(){
+    $(".navbar .nav-link").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function(){
+                window.location.hash = hash;
+            });
+        } 
+    });
+}); 
